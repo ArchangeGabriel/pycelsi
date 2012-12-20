cb07b67c2ba4e0d11a0a868b110259007524f778
Denis Ibanez <denis.ibanez@ens-lyon.fr>
2012-12-20 12:29:48 +0100

    On continue d'assainir le code. animation.py n'importe plus le main. Les actions principales ont emmigré dans le main, comme le veut la coutume. Un fichier parametre.txt permet d'amorcer le système - à faire - et de gérer les paramètres d'affichage - fonctionnel.

--- animation.py
+++ animation.py
@@ -5,11 +5,16 @@
 Fichier en charge de l'affichage et de l'animation du système
 """
 
+
+import numpy
 import matplotlib.pyplot as pyplot
-import matplotlib.animation
-from systeme import *
-from main import *
-from time import time
+
+
+#obtention des paramètres de l'animation et du système
+parametres = numpy.genfromtxt('parametres.txt')
+temps_relat = parametres[parametres.shape[0]-3]
+periode_affichage = parametres[parametres.shape[0]-2]
+calc_par_frame = int(parametres[parametres.shape[0]-1])
 
 
 #création de la figure qui sera affichée
@@ -26,13 +31,14 @@ def initialisation():
     """
     Initialise une première frame vide pour donner à FuncAnimation les objets à modifier à chaque frame.
     """
+
     univers.set_data([], [])
     energie_texte.set_text('')
     
     return univers, energie_texte
 
 
-def animer(i):
+def animer(i, calc_par_frame, systeme, dt):
     """
     Génère l'animation de la frame i en actualisant les positions et l'énergie de la frame i-1 et en envoyant le résultat dans l'"univers visuel"
     """
@@ -40,32 +46,18 @@ def animer(i):
     for calc in range (0,calc_par_frame) :
         systeme.iteration(dt)
 
-#    print systeme.positions()
-
-    univers.set_data(*systeme.positions()) #l'étoile spécifie à la méthode qu'on utilise un argument qui a un nom. Sinon elle crie
+    univers.set_data(*systeme.positions()) #l'étoile spécifie à la méthode qu'on utilise un argument qui "a un nom"
     energie_texte.set_text('energie = %.3e J' % (systeme.E_T+systeme.E_V)) #j'ai pensé que 3 décimales suffisaient
 
     return univers, energie_texte
 
 
-#on peut fixer l'intervalle en fonction de la période d'affichage souhaitée et de la durée d'affichage d'une frame, histoire d'en être indépendant
-dt = periode_affichage*temps_relat/calc_par_frame #on fixe la période de calcul
-from time import time
-t0 = time()
-animer(0)
-t1 = time()
-assert periode_affichage - (t1 - t0) > 0, "période d'affichage demandée %f s trop faible par rapport au temps d'affichage %f s" % (periode_affichage , t1-t0) # on ne pourra pas réduire la période de refresh en-dessous du temps nécessaire à l'affichage
-periode_affichage = periode_affichage - (t1 - t0) #on adapte la période d'affichage pour coller au temps spécifié initialement
 
 
-#animation et affichage à proprement parler
-anim = matplotlib.animation.FuncAnimation (fig, animer, frames=300, interval=periode_affichage*1000, blit=True, init_func=initialisation)
-pyplot.show()
+#-plus bas ; partie obsolète codée avec les pieds utilisée pour les tests-
 
 
-if __name__=="__main__": #partie codée avec les pieds à utiliser pour les tests
-
-    import numpy
+if __name__=="__main__":
 
     class corps(object):
         def energie(self):
--- main.py
+++ main.py
@@ -7,19 +7,48 @@ import numpy
 UA = 1.49597871e11 #une UA en m
 
 
+
+
 #---------------------------création du système--------------------------- version d'essai
+
 
 #systeme = Systeme([['soleil',0,0,0,0,1.392684e9/2,1.9891e30],['terre',1*UA,0,0,2*numpy.pi*1*UA/365.25696/24/3600,6.3710e6,5.9736e24]])
 systeme= Systeme ([['soleil',0,0,0,0,0.1,6.573491451e10],['terre',0,-1,2*numpy.pi/3,0,0.1,1e1],['terre',0,1,-2*numpy.pi/3,0,0.1,1e1]])
 
 
-#--------------------réglage de la vitesse d'affichage--------------------
-
-temps_relat = 0.1 #coefficient d'écoulement du temps par rapport au temps réel
-periode_affichage = 0.025 #en s, période d'affichage d'une nouvelle image
-calc_par_frame = 5 #nombre de calculs par image
 
 
 #-------------------------affichage et animations-------------------------
 
+
+import matplotlib.pyplot as pyplot
+import matplotlib.animation
+from time import time
+
 from animation import *
+
+
+
+
+#--------réglage de la vitesse d'affichage--------------------------------
+
+
+#on fixe l'intervalle en fonction de la période d'affichage souhaitée et de la durée d'affichage d'une frame, histoire d'en être indépendant
+dt = periode_affichage*temps_relat/calc_par_frame #on fixe la période de calcul
+
+t0 = time()
+animer(0, calc_par_frame, systeme, dt)
+t1 = time()
+
+assert periode_affichage - (t1 - t0) > 0, "période d'affichage demandée %f s trop faible par rapport au temps d'affichage %f s" % (periode_affichage , t1-t0) # on ne pourra pas réduire la période de refresh en-dessous du temps nécessaire à l'affichage
+
+periode_affichage = periode_affichage - (t1 - t0) #on adapte la période d'affichage pour coller au temps spécifié initialement
+
+
+
+
+#--------animation du système---------------------------------------------
+
+
+anim = matplotlib.animation.FuncAnimation (fig, animer, fargs=(calc_par_frame, systeme, dt), frames=300, interval=periode_affichage*1000, blit=True, init_func=initialisation)
+pyplot.show()
