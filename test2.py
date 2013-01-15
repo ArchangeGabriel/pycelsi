baa29f19c646c2e5e7f4453e7353c012c23c8046
Bruno Pagani <bruno.n.pagani@gmail.com>
2013-01-15 07:34:52 -0800

    Deuxième exemple foireux

---
+++ test2.py
@@ -0,0 +1,22 @@
+from scipy.integrate import odeint
+
+y0_0 = 1
+y1_0 = 0
+z = [y0_0, y1_0]
+
+def func(z, t):
+
+    t = z[0]
+
+    z[0] = z[1]
+    z[-1] = t
+
+    return z
+
+dt = 0.1
+
+z = odeint(func, z, [0, dt])
+print z
+
+z = odeint(func, z[1], [0, dt])
+print z
